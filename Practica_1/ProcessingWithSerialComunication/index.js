8a51406d292c2032344224a8e61daaaf3fd7d3f0
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-02-09 14:47:07 -0600

    Serial comunication

---
+++ Practica_1/ProcessingWithSerialComunication/index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { SerialPort, ReadlineParser } = require('serialport');
+const socketio = require('socket.io');
+const http = require('http');
+
+const parser = new ReadlineParser();
+
+const port = new SerialPort({
+    path: 'COM3',
+    baudRate: 9600,
+    autoOpen: false
+  })
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(morgan('dev'));
+app.use(cors());
+const portNumber = process.env.PORT;
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(portNumber, () => {
+    console.log("Servidor corriendo en el puerto", portNumber);
+});
+        
+
+port.open(function (err) {
+    if (err) {
+        return console.log('Error opening port: ', err.message)
+    }
+    else
+    {
+        return console.log("Pueto abierto correctamente");
+    }
+})
+
+port.pipe(parser);
+    parser.on('data',(data) => {
+    console.log(data);
+    io.emit('data', data);
+}); 
---
+++ Practica_1/ProcessingWithSerialComunication/public/main.js
@@ -0,0 +1,168 @@
+const display = document.querySelector('#display');
+
+const socket = io();
+
+let txtPrueba = ""
+
+socket.on('data', (data) => {
+    console.log("hola"+data);
+    // display.innerHTML = data;
+    txtPrueba = data;
+});
+
+// ACA EMPIEZA EL CODIGO DE P5.JS TUYO RAIDRI
+
+let img, img2, img3, img4;
+let button;
+function setup() {
+    // put setup code here
+    createCanvas(windowWidth,windowHeight);
+    background(0,204,204);
+    img = createImg('./3534501.png','temperature');
+    img2 = createImg('./humedad.png','humedad');
+    img3 = createImg('./aire.png','aire');
+    img4 = createImg('./presion.png','presion');
+}
+
+function draw() {
+  // TITULO PRINCIPAL
+  textSize(30);
+  textAlign(LEFT);
+  fill(255,255,255);
+  text("Estación Meteorológica IOT - Grupo 9",50,50)
+
+  //TEMPERATURA
+  //-RECTANGULO
+  fill('#063564');
+  noStroke();
+  rect(50, 90, 400, 180, 20);
+  describe(
+    'Rectángulo de información de la temperatura', 
+  );
+
+  //-IMAGEN
+  
+  image(img,50,100,150,150);
+  
+  //-TEXTO
+  textSize(30);
+  fill(255,255,255);
+  text("Temperatura",220,150);
+  textSize(50);
+  fill(255,255,255);
+  text(txtPrueba,250,220);
+
+  //HUMEDAD RELATIVA
+  //-RECTANGULO
+  fill('#063564');
+  noStroke();
+  rect(500, 90, 400, 180, 20);
+  describe(
+    'Rectángulo de información de la humedad', 
+  );
+
+  //-IMAGEN
+  
+  image(img2,500,100,150,150);
+  
+  //-TEXTO
+  textSize(30);
+  fill(255,255,255);
+  text("Humedad relativa",640,150);
+  textSize(50);
+  fill(255,255,255);
+  text("25ºC",710,220);
+
+  //HUMEDAD ABSOLUTA
+  //-RECTANGULO
+  fill('#063564');
+  noStroke();
+  rect(950, 90, 400, 180, 20);
+  describe(
+    'Rectángulo de información de la humedad', 
+  );
+
+  //-IMAGEN
+  
+  image(img2,950,100,150,150);
+  
+  //-TEXTO
+  textSize(27);
+  fill(255,255,255);
+  text("Humedad Absoluta",1090,150);
+  textSize(50);
+  fill(255,255,255);
+  text("25g/m",1110,220);
+  textSize(20);
+  text("3",1250,190);
+
+  //VELOCIDAD DEL VIENTO
+  //-RECTANGULO
+  fill('#063564');
+  noStroke();
+  rect(50, 300, 400, 180, 20);
+  describe(
+    'Rectángulo de información de la velocidad del viento', 
+  );
+
+  //-IMAGEN
+  
+  image(img3,75,325,120,120);
+  
+  //-TEXTO
+  textSize(25);
+  fill(255,255,255);
+  text("Velocidad del Viento",200,350);
+  textSize(50);
+  fill(255,255,255);
+  text("25km/h",250,420);
+
+  //DIRECCION DEL VIENTO
+  //-RECTANGULO
+  fill('#063564');
+  noStroke();
+  rect(500, 300, 400, 180, 20);
+  describe(
+    'Rectángulo de información de la humedad', 
+  );
+
+  //-IMAGEN
+  
+  image(img3,520,325,120,120);
+  
+  //-TEXTO
+  textSize(25);
+  fill(255,255,255);
+  text("Dirección del Viento",650,350);
+  textSize(50);
+  fill(255,255,255);
+  text("Sur",700,420);
+
+  //PRESIÓN BAROMÉTRICA
+  //-RECTANGULO
+  fill('#063564');
+  noStroke();
+  rect(950, 300, 400, 180, 20);
+  describe(
+    'Rectángulo de información de la humedad', 
+  );
+
+  //-IMAGEN
+  
+  image(img4,975,335,100,100);
+  
+  //-TEXTO
+  textSize(27);
+  fill(255,255,255);
+  text("Presión Barométrica",1090,350);
+  textSize(50);
+  fill(255,255,255);
+  text("25mmHg",1110,420);
+  textSize(20);
+  text("3",1250,190);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0,204,204);
+}
