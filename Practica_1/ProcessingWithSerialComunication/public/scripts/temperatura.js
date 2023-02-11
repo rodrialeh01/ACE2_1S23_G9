fafde32132b9ce5ac430953811a22450151219b6
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-02-11 00:46:18 -0600

    get data api

--- Practica_1/ProcessingWithSerialComunication/public/scripts/temperatura.js
+++ Practica_1/ProcessingWithSerialComunication/public/scripts/temperatura.js
@@ -5,3 +5,16 @@ let txtPrueba = ""
 socket.on('data', (data) => {
     console.log("hola"+data);
 });
+
+const consulta = () => {
+    fetch(`http://localhost:3000/data/T`, {
+    method: 'GET',
+    headers:{
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',}})
+    .then(res => console.log(res.json()))
+    .catch(err => {
+        console.error('Error:', err)
+        alert("Ocurrio un error, ver la consola")
+    });
+  };
