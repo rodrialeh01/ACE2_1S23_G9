798db2a4cadf16f413de187ac0d991864dca257d
Rodrigo Hernández <2823897250101@ingenieria.usac.edu.gt>
2023-02-12 17:54:21 -0600

    Enviando el correcto xd

---
+++ Practica_1/Arduino 2/sketch_feb10a/sketch_feb10a.ino
@@ -0,0 +1,61 @@
+#include <Wire.h>		// incluye libreria de bus I2C
+#include <Adafruit_Sensor.h>	// incluye librerias para sensor BMP280
+#include <Adafruit_BMP280.h>
+const int sensorPin = 9;
+int encoder_pin = 2;             //Pin 2, donde se conecta el encoder       
+unsigned int rpm = 0;           // Revoluciones por minuto calculadas.
+float velocity = 0;                 //Velocidad en [Km/h]
+volatile byte pulses = 0;       // Número de pulsos leidos por el Arduino en un segundo
+unsigned long timeold = 0;  // Tiempo 
+unsigned int pulsesperturn = 20; // Número de muescas que tiene el disco del encoder.
+const int wheel_diameter = 25;   // Diámetro de la rueda pequeña[mm]
+static volatile unsigned long debounce = 0; // Tiempo del rebote.
+
+Adafruit_BMP280 bmp;		// crea objeto con nombre bmp
+
+float TEMPERATURA;		// variable para almacenar valor de temperatura
+float PRESION, P0;		// variables para almacenar valor de presion atmosferica
+				// y presion actual como referencia para altitud
+
+void setup() {
+  Serial.begin(9600);				// inicializa comunicacion serie a 9600 bps
+  if ( !bmp.begin() ) {				// si falla la comunicacion con el sensor mostrar
+    Serial.println("BMP280 no encontrado !");	// texto y detener flujo del programa
+    while (1);					// mediante bucle infinito
+  }
+  pinMode(encoder_pin, INPUT); // Configuración del pin nº2
+   attachInterrupt(0, counter, RISING); // Configuración de la interrupción 0, donde esta conectado. 
+   pulses = 0;
+   rpm = 0;
+   timeold = 0;
+}						// en hectopascales para calculo de altitud relativa
+
+void loop() {
+  TEMPERATURA = bmp.readTemperature();		// almacena en variable el valor de temperatura
+  PRESION = (bmp.readPressure()/101325)*760;		// almacena en variable el valor de presion divido
+						// por 100 para covertirlo a hectopascales
+  Serial.print("P/");			// muestra texto
+  Serial.println(PRESION);			// muestra valor de la variable
+
+  if (millis() - timeold >= 1000){  // Se actualiza cada segundo
+      noInterrupts(); //Don't process interrupts during calculations // Desconectamos la interrupción para que no actué en esta parte del programa.
+      rpm = (60 * 1000 / pulsesperturn )/ (millis() - timeold)* pulses; // Calculamos las revoluciones por minuto
+      velocity = rpm * 3.1416 * wheel_diameter * 60 / 1000000; // Cálculo de la velocidad en [Km/h] 
+      timeold = millis(); // Almacenamos el tiempo actual.
+      Serial.print("V/");      
+      Serial.println(velocity,2); 
+      pulses = 0;  // Inicializamos los pulsos.
+      interrupts(); // Restart the interrupt processing // Reiniciamos la interrupción
+   }
+
+  delay(500);					// demora de 5 segundos entre lecturas
+}
+
+////Fin de programa principal //////////////////////////////////////////////////////////////////////////////////
+///////////////////////////Función que cuenta los pulsos buenos ///////////////////////////////////////////
+ void counter(){
+  if(  digitalRead (encoder_pin) && (micros()-debounce > 500) && digitalRead (encoder_pin) ) { 
+// Vuelve a comprobar que el encoder envia una señal buena y luego comprueba que el tiempo es superior a 1000 microsegundos y vuelve a comprobar que la señal es correcta.
+        debounce = micros(); // Almacena el tiempo para comprobar que no contamos el rebote que hay en la señal.
+        pulses++;}  // Suma el pulso bueno que entra.
+        else ; } 
