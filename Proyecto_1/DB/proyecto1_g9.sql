75e6c23ee8cdd6841bc53ae69a8f656317ac3720
rodrialeh01 <rodrialehdl@gmail.com>
2023-03-22 00:45:40 -0600

    Actualizacion del Login en PWA con BD

--- Proyecto_1/DB/proyecto1_g9.sql
+++ Proyecto_1/DB/proyecto1_g9.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-03-2023 a las 04:55:55
+-- Tiempo de generación: 22-03-2023 a las 07:14:58
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.0.25
 
@@ -35,6 +35,17 @@ CREATE TABLE `configuracion` (
   `tiempo_sistema` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `configuracion`
+--
+
+INSERT INTO `configuracion` (`id_usuario`, `nombre`, `tiempo_trabajo`, `tiempo_descanso`, `tiempo_sistema`) VALUES
+(1, 'Daniel', 25, 5, '2023-03-22 05:20:23'),
+(2, 'Reginaldo', 25, 5, '2023-03-22 05:20:39'),
+(3, 'Dubon', 25, 5, '2023-03-22 05:20:54'),
+(4, 'Rodriguez', 25, 5, '2023-03-22 05:21:06'),
+(5, 'Prueba', 25, 5, '2023-03-22 05:21:43');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +60,24 @@ CREATE TABLE `datos` (
   `id_pomodoro` int(11) NOT NULL,
   `fase_pomodoro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id`, `estado`) VALUES
+(1, 0);
 
 --
 -- Índices para tablas volcadas
@@ -68,6 +97,12 @@ ALTER TABLE `datos`
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
+-- Indices de la tabla `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -75,13 +110,19 @@ ALTER TABLE `datos`
 -- AUTO_INCREMENT de la tabla `configuracion`
 --
 ALTER TABLE `configuracion`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `datos`
 --
 ALTER TABLE `datos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
--- Proyecto_1/PWA/assets/js/register.js
+++ Proyecto_1/PWA/assets/js/register.js
@@ -4,6 +4,7 @@ function validateName(){
       console.log("name: ", name);
       localStorage.setItem('name', name);
       RegistrarUsuario(name);
+      setLogueado();
       window.location.href = './Dashboard.html';
     }else{
       alert('No se puede ingresar la aplicación sin ingresar su Nombre')
@@ -29,7 +30,27 @@ function RegistrarUsuario(user){
     .catch(err => {
       console.error('Error:', err)
       alert(err);
+
       //alert("Ocurrio un error, ver la consola")
+    })
+}
+
+function setLogueado(){
+  let logueado = {
+    "estado": 1
+  }
+  fetch(`http://192.168.0.2:4000/updateLogin`, {
+    method: 'PUT',
+    body: JSON.stringify(logueado),
+    headers:{
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',}})
+    .then(res => res.json())
+    .catch(err => {
+        console.error('Error:', err)
+    })
+    .then(response =>{
+        console.log(response);
     })
 }
 
