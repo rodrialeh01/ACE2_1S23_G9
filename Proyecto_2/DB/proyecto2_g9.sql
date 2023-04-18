01c8f3cb14c8ce421809d7a6026423579e39b6c5
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-04-18 01:12:02 -0600

    DB

---
+++ Proyecto_2/DB/proyecto2_g9.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-04-2023 a las 09:08:50
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto2_g9`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sensores`
+--
+
+CREATE TABLE `sensores` (
+  `id` int(11) NOT NULL,
+  `humedad` int(11) NOT NULL,
+  `tmp_int` int(11) NOT NULL,
+  `tmp_ext` int(11) NOT NULL,
+  `tiempo` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `sensores`
+--
+
+INSERT INTO `sensores` (`id`, `humedad`, `tmp_int`, `tmp_ext`, `tiempo`) VALUES
+(1, 5, 10, 15, '2023-04-18 07:01:49');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `sensores`
+--
+ALTER TABLE `sensores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `sensores`
+--
+ALTER TABLE `sensores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
