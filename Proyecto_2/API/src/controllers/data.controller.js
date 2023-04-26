1d903ad677f6969c621aa6a7fa4edf12061ba473
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-04-25 21:02:48 -0600

    update API

--- Proyecto_2/API/src/controllers/data.controller.js
+++ Proyecto_2/API/src/controllers/data.controller.js
@@ -4,7 +4,7 @@ export const setSensors = async (req, res) =>
 {
     const { humedad, tmp_int, tmp_ext, pr_agua, est_bomba } = req.body;
     const [rows] = await pool.query("INSERT INTO sensores (humedad, tmp_int, tmp_ext, pr_agua, est_bomba) VALUES (?, ?, ?, ?, ?)", [humedad, tmp_int, tmp_ext, pr_agua, est_bomba]);
-    const [rows2] = await pool.query("UPDATE control SET est_bomba = ? WHERE id = 1", [est_bomba]);
+    const [rows2] = await pool.query("UPDATE control SET est_bomba = ?, humedad = ? WHERE id = 1", [est_bomba, humedad]);
     res.send("OK");
 };
 
@@ -35,4 +35,15 @@ export const filtarAgua = async (req, res) =>
     const { fechaInicio, fechaFinal } = req.body;
     const [rows] = await pool.query("SELECT pr_agua FROM sensores WHERE (tiempo BETWEEN ? AND ?)", [ fechaInicio, fechaFinal ]);
     res.send(rows);
+}
+
+export const getControl = async (req, res) => {
+    const [rows] = await pool.query("SELECT est_bomba, tmp_conf, tmp_act, alerta, humedad FROM control WHERE id = 1");
+    res.send(rows);
+}
+
+export const setControlApk = async (req, res) => {
+    const { tmp_conf, tmp_act, est_bomba } = req.body;
+    const [rows] = await pool.query("UPDATE control SET tmp_conf = ?, tmp_act = ?, est_bomba = ? WHERE id = 1", [tmp_conf, tmp_act, est_bomba]);
+    res.send("OK");
 }
--- Proyecto_2/API/src/routes/data.routes.js
+++ Proyecto_2/API/src/routes/data.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { filtarAgua, filtarHumedad, filtarTmpExt, filtarTmpInt, setSensors } from "../controllers/data.controller.js";
+import { filtarAgua, filtarHumedad, filtarTmpExt, filtarTmpInt, getControl, setControlApk, setSensors } from "../controllers/data.controller.js";
 
 const router = Router();
 
@@ -12,5 +12,9 @@ router.post("/filtroTmpExt", filtarTmpExt);
 router.post("/filtarHumedad", filtarHumedad);
 
 router.post("/filtarAgua", filtarAgua);
+
+router.get("/getControl", getControl);
+
+router.post("/setControlApk", setControlApk);
 
 export default router;
